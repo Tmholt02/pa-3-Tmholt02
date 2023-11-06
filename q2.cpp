93b56726e085933e63cfa10d33df627a27a315b5
Thomas Holt <tmholt02@gmail.com>
2023-11-05 21:47:22 -0600

    Updated time measurement

--- q2.cpp
+++ q2.cpp
@@ -3,38 +3,40 @@
 #include <unistd.h>
 
 void getpidWithOverhead() {
-    getpid();
-}
-
-long long getTimeDiff() {
-    struct timespec start, end;
-    long long functionCallTime, systemCallTime;
-
-    // Measure time taken for the simple function call
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    getpidWithOverhead();
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    functionCallTime = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
-
-    // Measure time taken for the getpid system call
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    getpid();
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    systemCallTime = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
-
-    return functionCallTime - systemCallTime;
+    // getpid();
 }
 
 int main() {
-    int n = 100000;
-    double avgDiff = 0.0;
+
+    const int n = 1000000;
+    double avgFunc = 0.0;
+    double avgSysC = 0.0;
+
     for (int i = 0; i < n; i++) {
-        avgDiff += getTimeDiff() / (double)n;
+
+        struct timespec start, end;
+        long long functionCallTime, systemCallTime;
+
+        // Measure time taken for the simple function call
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        getpidWithOverhead();
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        functionCallTime = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
+
+        // Measure time taken for the getpid system call
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        getpid();
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        systemCallTime = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
+
+        avgFunc += functionCallTime / (double)n;
+        avgSysC += systemCallTime / (double)n;
     }
 
-
     // Output the results
-    printf("Function overhead adds %2f ns on average.\n", avgDiff);
+    printf("Function calls take %.2f ns on average.\n", avgFunc);
+    printf("GetPID system call takes %.2f ns on average.\n", avgSysC);
+    printf("Syscall is %.2f ns slower on average.\n", avgSysC - avgFunc);
 
     return 0;
 }
